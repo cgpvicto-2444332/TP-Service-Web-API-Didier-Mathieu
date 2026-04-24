7b666893170599bfe7f2601291a13e02cb202dc3
Didier Mathieu <dmathieu2007@gmail.com>
2026-04-24 18:07:13 -0400

    séparation des requetes pour utilisateur dans un fichier à part

--- src/models/bibliotheque.model.js
+++ src/models/bibliotheque.model.js
@@ -73,25 +73,3 @@ const _modifierStatusPret = async (id, status) => {
  */
 const _supprimerPret = async (id) => {
 };
-
-/**
- * Ajoute un utilisateur et lui donne une clé d'api pour se connecter au routes.
- * @param {object} utilisateur Les données du nouvel utilisateur.
- */
-const _ajouterUtilisateur = async (utilisateur) => {
-};
-
-/**
- * Récupère la clé d'api en fonction du courriel d'un utilisateur si le mot de passe et courriel sont valides.
- * @param {string} courriel Le courriel de l'utilisateur à vérifier.
- */
-const _recupererUtilisateur = async (courriel) => {
-};
-
-/**
- * Génère une nouvelle clé d'api en fonction du id de l'utilisateur
- * @param {string} nouvelleCle La nouvelle clé api à remplacer dans la bdé
- * @param {number} id L'identifiant de l'utilisateur.
- */
-const _nouvelleCle = async (nouvelleCle, id) => {
-};
---
+++ src/models/utilisateur.model.js
@@ -0,0 +1,23 @@
+import pool from "../config/db_pg.js";
+
+/**
+ * Ajoute un utilisateur et lui donne une clé d'api pour se connecter au routes.
+ * @param {object} utilisateur Les données du nouvel utilisateur.
+ */
+const _ajouterUtilisateur = async (utilisateur) => {
+};
+
+/**
+ * Récupère la clé d'api en fonction du courriel d'un utilisateur si le mot de passe et courriel sont valides.
+ * @param {string} courriel Le courriel de l'utilisateur à vérifier.
+ */
+const _recupererUtilisateur = async (courriel) => {
+};
+
+/**
+ * Génère une nouvelle clé d'api en fonction du id de l'utilisateur
+ * @param {string} nouvelleCle La nouvelle clé api à remplacer dans la bdé
+ * @param {number} id L'identifiant de l'utilisateur.
+ */
+const _nouvelleCle = async (nouvelleCle, id) => {
+};
