a01c8b465ed3e5b13d07684465a076cee99e83ce
Didier Mathieu <dmathieu2007@gmail.com>
2026-04-25 12:48:07 -0400

    squelette des controllers des utilisateurs et de la bibliotheque

---
+++ src/controllers/bibliotheque.controller.js
@@ -0,0 +1,25 @@
+import * as bibliothequeModele from '../models/bibliotheque.model.js';
+
+export const getListeLivres = async (req, res) => {
+};
+
+export const getListeById = async (req, res) => {
+};
+
+export const ajouterLivre = async (req, res) => {
+};
+
+export const modifierLivre = async (req, res) => {
+};
+
+export const supprimerLivre = async (req, res) => {
+};
+
+export const ajouterPret = async (req, res) => {
+};
+
+export const modifierPret = async (req, res) => {
+};
+
+export const supprimerPret = async(req, res) => {
+};
---
+++ src/controllers/utilisateur.controller.js
@@ -0,0 +1,7 @@
+import * as utilisateursModel from '../models/utilisateurs.model.js';
+
+export const ajouterUtilisateur = async (req, res) => {
+};
+
+export const recupererCle = async (req, res) => {
+};
